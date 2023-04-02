2ca680183abf35344163a4ea719f70ffbd1e0b3c
litu <dipti.90988@gmail.com>
2023-04-02 22:36:26 +0530

    This is commit by litu

--- src/test/java/MVNTest2/FourTest.java
+++ src/test/java/MVNTest2/FourTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class FourTest {
 	@Test
 	public void mFour() {
-		System.out.println("mFour Executed");
+		System.out.println("litu has updated this code from github");
 	}
 
 }
